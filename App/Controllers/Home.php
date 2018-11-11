702e546514ce7ef1f4f07fc7dabc5084166f0fd4
emreincu <ynsemreincu@gmail.com>
2018-11-11 14:43:56 +0300

    I will rebuild this project!

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -1,10 +1,14 @@
 <?php
-namespace app\controllers;
+namespace App\Controllers;
 use Core\Controller;
 
 class Home extends Controller {
+
     public function __construct($controller, $action) {
         parent::__construct($controller, $action);
+    }
+    public function index() {
+
     }
 
 }

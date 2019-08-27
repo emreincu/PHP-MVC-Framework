34e5c709cf92b5df5e5644c0a5f6880ee7f350cc
Emre <ynsemreincu@gmail.com>
2019-08-27 15:14:29 +0300

    Router class refactored

---
+++ App/APIs/Example_API.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\APIs;
+
+class Example_API {
+    public function test_action() {
+        echo "test";
+    }
+}
--- Core/Router.php
+++ Core/Router.php
@@ -4,76 +4,64 @@ namespace Core;
 
 use Core\Language;
 
+/**
+ * Class Router
+ */
 class Router {
     
     private $className;
     private $actionName;
-   
-    public static function route($url) {
+
+   /**
+    * Function route: this function routing url
+    * @param string $url
+    * @return void
+    */
+    public static function route(array $url) {
+
+        /**
+         * feature-notice: i will add here the annotation.
+         */
 
         $class = (isset($url[0]) && $url[0] != '') ? $url[0] : DEFAULT_CONTROLLER;
         $class = ucwords($class);
+        $namespace = "";
+
+        $default_class = DEFAULT_CONTROLLER;
+        $default_action = DEFAULT_ACTION;
 
         if($class === "Api") {
             array_shift($url);
+            $namespace .= '\\App\\APIs\\';
+            $default_class = DEFAULT_API;
+        }else if($class === "Language"){
+            $namespace .= '\\Core\\';
+            $default_class = "Language";
+        }else{
+            $namespace .= '\\App\\Controllers\\';
+            $default_class = DEFAULT_CONTROLLER;
+        }
 
-            $action = (isset($url[0]) && $url[0] != '') ? $url[0] : DEFAULT_ACTION;
-            $action = ucwords($action);
+        $class = (isset($url[0]) && $url[0] != '') ? $url[0] : $default_class;
+        $class = $namespace . ucwords($class);
 
-            $class = '\\App\\APIs\\'.$action;
-            array_shift($url);
+        array_shift($url);
+        $action = (isset($url[0]) && $url[0] != '') ? $url[0] : $default_action;
+        
+        array_shift($url);
+        $parameters = $url;
 
-            $action = (isset($url[0]) && $url[0] != '') ? $url[0] : DEFAULT_ACTION;
-            array_shift($url);
-
-            $parameters = $url;
-
-            if(class_exists($class)){
-                $dispatch = new $class($class, $action);
-                if(method_exists($class, $action)) {
-                    Language::init();
-                    call_user_func_array([$dispatch, $action], $parameters);
-                } else {
-                    Response::setHeader("Access-Control-Allow-Origin" , "*");
-                    Response::setHeader("Content-Type" , "application/json; charset=UTF-8");
-                    Response::setHeader("Access-Control-Allow-Credentials" , "true");
-                    Response::setHeader("Content-Type" , "application/json");
-                    Response::setStatus(404);
-                }
-            }else{
-                Response::setHeader("Access-Control-Allow-Origin" , "*");
-                Response::setHeader("Content-Type" , "application/json; charset=UTF-8");
-                Response::setHeader("Access-Control-Allow-Credentials" , "true");
-                Response::setHeader("Content-Type" , "application/json");
-                Response::setStatus(404);
+        if(class_exists($class)){
+            $dispatch = new $class($class, $action);
+            if(method_exists($class, $action)) {
+                Language::init();
+                call_user_func_array([$dispatch, $action], $parameters);
+            } else {
+                die("That method does not exists in the controller!");
             }
         }else{
-            if($class === "Language") {
-                $class = '\\Core\\Language';
-            }else{
-                $class = '\\App\\Controllers\\'.$class;
-            }
-            
-            array_shift($url);
-            $action = (isset($url[0]) && $url[0] != '') ? $url[0] : DEFAULT_ACTION;
-            
-            array_shift($url);
-            $parameters = $url;
-
-            if(class_exists($class)){
-                $dispatch = new $class($class, $action);
-                if(method_exists($class, $action)) {
-                    Language::init();
-                    call_user_func_array([$dispatch, $action], $parameters);
-                } else {
-                    die("That method does not exists in the controller!");
-                }
-            }else{
-                die("That class does not exists in the controllers!");
-            }
+            die("That class does not exists in the controllers!");
         }
-        
-        
     }
 
     public function setClassName($className) {
